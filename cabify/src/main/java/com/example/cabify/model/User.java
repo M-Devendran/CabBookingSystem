6f53f3565d164b658cdeaf8bf32ef7c590ae8874
Abishek <2463393@cognizant.com>
2025-12-29 08:36:43 +0530

    Remove the setter and getter method

--- cabify/src/main/java/com/example/cabify/model/User.java
+++ cabify/src/main/java/com/example/cabify/model/User.java
@@ -16,66 +16,23 @@ import  lombok.Getter;
 @Entity
 @Table(name = "users")
 public class User {
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Column(unique = true, nullable = false)
     private String email;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     private String name;
     private long phone;
 
-    public long getPhone() {
-        return phone;
-    }
 
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 
     @Column(updatable = false)
     private LocalDateTime createdAt;
